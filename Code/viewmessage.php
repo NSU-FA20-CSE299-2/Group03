a84107a33a04d6785fb81418fdc5a7dbd3641c84
Saqif <saqif.mohtasim@northsouth.edu>
2021-01-02 20:09:56 +0600

    View message #24 : User can send messages.
    Users can see messages to the admin directly and it will save in the database.

---
+++ Code/viewmessage.php
@@ -0,0 +1,199 @@
+
+<?php
+
+include_once 'database.php';
+
+$result3 = mysqli_query($conn,"SELECT * FROM contact");
+
+session_start();
+$email = $_SESSION["email"];
+$admin = $_SESSION["utype"];
+
+
+if ($admin=="admin") {
+
+
+}
+else{
+		header("location: logout.php");
+	}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>VIEW MESSAGE</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+				<!-- CSS here -->
+				<link rel="stylesheet" href="css/bootstrap.min.css">
+				 <link rel="stylesheet" href="css/owl.carousel.min.css">
+				 <link rel="stylesheet" href="css/magnific-popup.css">
+				 <link rel="stylesheet" href="css/font-awesome.min.css">
+				 <link rel="stylesheet" href="css/themify-icons.css">
+				 <link rel="stylesheet" href="css/nice-select.css">
+				 <link rel="stylesheet" href="css/flaticon.css">
+				 <link rel="stylesheet" href="css/gijgo.css">
+				 <link rel="stylesheet" href="css/animate.min.css">
+				 <link rel="stylesheet" href="css/slicknav.css">
+
+				 <link rel="stylesheet" href="css/style.css">
+
+				 <link rel="stylesheet" href="css/custom.css">
+
+
+
+</head>
+<body>
+      <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container-fluid ">
+                    <div class="header_bottom_border">
+                        <div class="row align-items-center">
+                            <div class="col-xl-3 col-lg-2">
+                                <div class="logo">
+
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-7">
+                                <div class="main-menu  d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a href="admin.php">Admin panel</a></li>
+
+
+                                           <li><a href="viewmessage.php">View Message</a></li>
+                                                <li><a href="post_review.php">List a car</a></li>
+                                          <li><a href="logout.php">Logout</a></li>
+
+
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                                <div class="Appointment">
+
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+
+
+
+
+       <div class="bradcam_area_2 bradcam_bg_2">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-xl-5">
+                    <div class="bradcam_text">
+<h3 style="color: #485460" class="wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".2s"> User messages</h3>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+          </div>
+
+
+  <div class="container-fluid">
+
+        <div class="job_listing_area p-5">
+        <div class="container">
+
+            <div class="job_lists">
+                <div class="row">
+
+                   <?php
+                    $i=0;
+                    while($row = mysqli_fetch_array($result3)) {
+                    ?>
+
+					  <div class="col-lg-12 col-md-12">
+                        <div class="single_jobs white-bg d-flex justify-content-between">
+                            <div class="jobs_left d-flex align-items-center">
+
+                                    <img src="img/person.png" alt="">
+
+                                <div class="jobs_conetent">
+                             <h4><?php echo $row["message"];?></h4>
+                                    <div class="links_locat d-flex align-items-center">
+                                      <div class="location">
+                                            <p> <i class="fa fa-envelope" aria-hidden="true"></i> <?php echo $row["name"]; ?></p>
+                                        </div>
+                                       <div class="location">
+                                            <p><i class="fa fa-id-card" aria-hidden="true"></i> <?php echo $row["email"]; ?></p>
+                                        </div>
+                                        <div class="location">
+                                            <p> <i class="fa fa-key" aria-hidden="true"></i> <?php echo $row["phone"]; ?></p>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <?php
+                        $i++;
+                        }
+                        ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+      </div>
+    </div>
+
+  </body>
+
+
+
+
+
+		<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/isotope.pkgd.min.js"></script>
+		<script src="js/ajax-form.js"></script>
+		<script src="js/waypoints.min.js"></script>
+		<script src="js/jquery.counterup.min.js"></script>
+		<script src="js/imagesloaded.pkgd.min.js"></script>
+		<script src="js/scrollIt.js"></script>
+		<script src="js/jquery.scrollUp.min.js"></script>
+		<script src="js/wow.min.js"></script>
+
+		<script src="js/jquery.slicknav.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/plugins.js"></script>
+
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="js/main.js"></script>
+
+
+</html>
